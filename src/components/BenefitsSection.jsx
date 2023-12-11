8965b751dea93367e44e221899b85aec0c5f8228
TrojanDll <dzafarovt36@gmail.com>
2023-12-11 13:50:57 +0300

    fix: benefits renamed

--- src/components/BenefitsSection.jsx
+++ src/components/BenefitsSection.jsx
@@ -11,46 +11,46 @@ class BenefitsSection extends React.Component {
     const { classes } = this.props;
 
     return (
-      <section className={classes.benefits_wrapper}>
+      <section className={classes.benefitsWrapper}>
         <CustomContainer>
           <div className={classes.benefits}>
-            <h2 className={classes.benefits__title}>Better than manual</h2>
-            <div className={classes.benefits__descr}>
+            <h2 className={classes.title}>Better than manual</h2>
+            <div className={classes.descr}>
               You spend 4 times less time and get 7 times more leads compared to manual!
             </div>
 
-            <div className={classes.benefits__list}>
-              <div className={classes.benefits__listItem}>
-                <SpriteIcon id="icon-benefits-1" className={classes.benefits__listItem__img} />
-                <div className={classes.benefits__listItem__text}>
+            <div className={classes.list}>
+              <div className={classes.listItem}>
+                <SpriteIcon id="icon-benefits-1" className={classes.listItemImg} />
+                <div className={classes.listItemText}>
                   Saves you up to 75% of time on messaging your clients
                 </div>
               </div>
 
-              <div className={classes.benefits__listItem}>
-                <SpriteIcon id="icon-benefits-2" className={classes.benefits__listItem__img} />
-                <div className={classes.benefits__listItem__text}>
+              <div className={classes.listItem}>
+                <SpriteIcon id="icon-benefits-2" className={classes.listItemImg} />
+                <div className={classes.listItemText}>
                   Saves you 100% of time on statistics gathering
                 </div>
               </div>
 
-              <div className={classes.benefits__listItem}>
-                <SpriteIcon id="icon-benefits-3" className={classes.benefits__listItem__img} />
-                <div className={classes.benefits__listItem__text}>
+              <div className={classes.listItem}>
+                <SpriteIcon id="icon-benefits-3" className={classes.listItemImg} />
+                <div className={classes.listItemText}>
                   Doesn’t visit the same profiles again and again
                 </div>
               </div>
 
-              <div className={classes.benefits__listItem}>
-                <SpriteIcon id="icon-benefits-4" className={classes.benefits__listItem__img} />
-                <div className={classes.benefits__listItem__text}>
+              <div className={classes.listItem}>
+                <SpriteIcon id="icon-benefits-4" className={classes.listItemImg} />
+                <div className={classes.listItemText}>
                   Allows to work from 7+ accounts at the same time
                 </div>
               </div>
 
-              <div className={classes.benefits__listItem}>
-                <SpriteIcon id="icon-benefits-5" className={classes.benefits__listItem__img} />
-                <div className={classes.benefits__listItem__text}>
+              <div className={classes.listItem}>
+                <SpriteIcon id="icon-benefits-5" className={classes.listItemImg} />
+                <div className={classes.listItemText}>
                   Doesn’t message the same people from different accounts
                 </div>
               </div>
@@ -63,7 +63,7 @@ class BenefitsSection extends React.Component {
 }
 
 const styles = {
-  benefits_wrapper: {
+  benefitsWrapper: {
     paddingTop: '112px',
     backgroundColor: 'var(--lightest-grey-color)',
   },
@@ -75,34 +75,34 @@ const styles = {
     background: '#fff',
     filter: 'drop-shadow(0px 20px 60px rgba(0, 48, 71, 0.2))',
   },
-  benefits__title: {
+  title: {
     fontSize: '40px',
     fontWeight: 500,
     lineHeight: '48px',
     textAlign: 'center',
   },
-  benefits__descr: {
+  descr: {
     marginTop: '16px',
     fontSize: '24px',
     fontWeight: 300,
     lineHeight: '32px',
     textAlign: 'center',
   },
-  benefits__list: {
+  list: {
     marginTop: '44px',
     display: 'flex',
     justifyContent: 'space-between',
   },
-  benefits__listItem: {
+  listItem: {
     width: '189px',
   },
-  benefits__listItem__img: {
+  listItemImg: {
     display: 'block',
     margin: '0 auto',
     width: '100%',
     height: '112px',
   },
-  benefits__listItem__text: {
+  listItemText: {
     fontSize: '18px',
     fontWeight: 400,
     lineHeight: '24px',
@@ -113,47 +113,47 @@ const styles = {
     benefits: {
       padding: '64px 72px',
     },
-    benefits__listItem: {
+    listItem: {
       width: '140px',
     },
   },
 
   '@media (max-width: 992px)': {
-    benefits__list: {
+    list: {
       flexWrap: 'wrap',
       rowGap: '48px',
     },
-    benefits__listItem: {
+    listItem: {
       width: '100%',
     },
   },
   '@media (max-width: 768px)': {
-    benefits__title: {
+    title: {
       fontSize: '44px',
       lineHeight: '56px',
     },
-    benefits__descr: {
+    descr: {
       fontSize: '24px',
       lineHeight: '32px',
       fontWeight: 400,
     },
   },
   '@media (max-width: 576px)': {
-    benefits_wrapper: {
+    benefitsWrapper: {
       paddingTop: '64px',
     },
     benefits: {
       padding: '64px 30px',
     },
-    benefits__title: {
+    title: {
       fontSize: '32px',
       lineHeight: '40px',
     },
-    benefits__descr: {
+    descr: {
       fontSize: '18px',
       lineHeight: '24px',
     },
-    benefits__list: {
+    list: {
       rowGap: '32px',
     },
   },
